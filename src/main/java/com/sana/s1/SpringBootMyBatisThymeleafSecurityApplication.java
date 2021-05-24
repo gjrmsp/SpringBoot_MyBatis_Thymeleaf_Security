12550a11cb012fef5f541425321bb85161c186f5
gjrmsp <gjrmsp@naver.com>
2021-05-24 10:02:52 +0900

    Initial commit

---
+++ src/main/java/com/sana/s1/SpringBootMyBatisThymeleafSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sana.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisThymeleafSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyBatisThymeleafSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sana/s1/SpringBootMyBatisThymeleafSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sana.s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMyBatisThymeleafSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
